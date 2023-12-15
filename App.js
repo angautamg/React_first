5104be015ab4e3e06967a254dce7a33f21bef22e
Gautam <angautamg@gmail.com>
2023-12-16 01:12:03 +0530

    all commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myTemplate = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Welcome to my website</p>
+  </div>
+);
+
+ReactDOM.render(myTemplate, document.getElementById('root'));
